7f750f35343fe410478e9663a07988836e97ff98
michael-houze <michael.houze@loyalhealth.com>
2022-07-08 13:47:22 -0400

    added tester for sendgrid calls

---
+++ Tester.SendGrid/Program.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+// pull api key from launchSettings
+var apiKey = Environment.GetEnvironmentVariable("API_KEY");
+var client = new SendGridClient(apiKey);
+
+// build message
+var from = new EmailAddress("from@email.com", "From Name");
+var subject = "Test Email Subject Line";
+var to = new EmailAddress("to@email.com", "To Name");
+var plainTextContent = "The text/plain content of the email body.";
+var htmlContent = "The text/html content of the email body.";
+
+// send message
+var message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+var response = await client.SendEmailAsync(message);
+
+// print response
+if (response != null)
+{
+    Console.WriteLine($"SendGrid Response StatusCode: {response.StatusCode}");
+    Console.WriteLine($"SendGrid Response Body: {response.Body}");
+}
+else
+{
+    Console.WriteLine("SendGrid response was null");
+}
