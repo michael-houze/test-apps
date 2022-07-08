4b700a3ce0ea1108bbcfb3e3d57a1aab8ecc77de
michael-houze <michael.houze@loyalhealth.com>
2022-07-08 14:33:41 -0400

    added twilio tester

---
+++ Tester.Twilio/Program.cs
@@ -0,0 +1,28 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+// pull secrets from launchSettings
+var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+var fromNumber = Environment.GetEnvironmentVariable("TWILIO_FROM_NUMBER");
+
+TwilioClient.Init(accountSid, authToken);
+
+// build and send message
+var message = MessageResource.Create(
+    body: "I am not superstitious, but I am a little stitious.",
+    from: new PhoneNumber(fromNumber),
+    to: new PhoneNumber("+15558675309")
+);
+
+// print response
+if (message != null)
+{
+    Console.WriteLine($"Twilio Response StatusCode: {message.Status}");
+    Console.WriteLine($"Twilio Response Body: {message.Body}");
+}
+else
+{
+    Console.WriteLine("Twilio response was null");
+}
